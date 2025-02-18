89819a11f01ac961e9901685aaadcc28a50b2f7e
GilbertQ <iulisesq@hotmail.com>
2025-02-18 11:29:58 -0600

    4x4

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import "./Images.css"; // Ensure you create this CSS file
+import "./Images.css";
 
 const importAll = (context) => context.keys().map(context);
 
 const importAndSortImages = () => {
   const images = importAll(require.context('./toiles/', true, /\.(jpeg|jpg|png|gif)$/));
-
   return images.sort((a, b) => {
     const extractFilename = (path) => path.substring(path.lastIndexOf('/') + 1);
     const numA = parseInt(extractFilename(a).substring(0, 2), 10);
@@ -23,13 +22,16 @@ const ImageGallery = () => {
 
   const handleNext = () => {
     if (startIndex + maxVisibleThumbnails < sortedImages.length) {
-      setStartIndex(startIndex + 1);
+      setStartIndex(Math.min(
+        startIndex + maxVisibleThumbnails,
+        sortedImages.length - maxVisibleThumbnails
+      ));
     }
   };
 
   const handlePrev = () => {
     if (startIndex > 0) {
-      setStartIndex(startIndex - 1);
+      setStartIndex(Math.max(0, startIndex - maxVisibleThumbnails));
     }
   };
 
@@ -39,13 +41,17 @@ const ImageGallery = () => {
       <div className="main-image">
         <img src={selectedImage} alt="Selected Artwork" />
       </div>
-
       {/* Thumbnail Carousel */}
       <div className="thumbnail-carousel">
-        
-        
         <div className="thumbnails">
-        <button onClick={handlePrev} disabled={startIndex === 0} style={{ width: '75px' }}>&lt;</button>
+          <button 
+            onClick={handlePrev} 
+            disabled={startIndex === 0} 
+            className="nav-button"
+            style={{ width: '75px' }}
+          >
+            &lt;
+          </button>
           {sortedImages.slice(startIndex, startIndex + maxVisibleThumbnails).map((img, index) => (
             <img
               key={startIndex + index}
@@ -56,9 +62,15 @@ const ImageGallery = () => {
               title={img.slice(img.lastIndexOf("/") + 3).split(".")[0]}
             />
           ))}
-          <button onClick={handleNext} disabled={startIndex + maxVisibleThumbnails >= sortedImages.length} style={{ width: '75px' }}>&gt;</button>
+          <button 
+            onClick={handleNext} 
+            disabled={startIndex + maxVisibleThumbnails >= sortedImages.length}
+            className="nav-button"
+            style={{ width: '75px' }}
+          >
+            &gt;
+          </button>
         </div>
-
       </div>
     </div>
   );
